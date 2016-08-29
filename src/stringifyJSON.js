3259102597740974b975acebbde2d33033d14cae
Eric Eakin <eseakin@gmail.com>
2016-08-28 20:13:55 -0400

    In progress

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,55 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var toStr = [1,true,undefined,'text',[1,{a:1,b:2},[1,2]],{a:[1,2],b:{a:1,b:2}}];
+
+var stringifyJSON = function(obj, newStr) {
+	console.log("OBJ ",obj);
+
+	newStr = newStr || "";
+
+	obj.forEach(function(ele,i,col){
+
+		//adds commas
+		if(i >= 1 && i <= col.length){
+			newStr += ",";
+		}
+
+		//if array
+		if(Array.isArray(ele)){
+			newStr += "[";
+
+			stringifyJSON(ele,newStr);
+
+			newStr +=  "]";
+		}
+
+		//if object
+		else if(typeof ele === "object"){
+			newStr += "{";
+
+			stringifyJSON(ele,newStr);
+
+			newStr += "}";
+		}
+
+		//regular expressions
+		else if(ele === undefined || ele === null){
+			newStr += "null";
+		}else if(ele === false){
+			newStr += "false";
+		}else if(ele === true){
+			newStr += "true";
+		}else if(typeof ele === "string"){
+			newStr += "\"" + ele + "\"";
+		}else if(typeof ele === "number"){
+			newStr += ele;
+		}
+	});
+	return newStr;
+
+
+
 };
+	console.log("STRING TEST ",stringifyJSON(toStr));
+	console.log("STRINGIFY ",JSON.stringify(toStr));
