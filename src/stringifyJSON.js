ab28ed838397f190792d196d0726ee406ab6e3df
Eric Eakin <eseakin@gmail.com>
2016-08-28 22:39:50 -0400

    Assignment COMPLETE

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,55 +3,165 @@
 
 // but you don't so you're going to write it from scratch:
 
-var toStr = [1,true,undefined,'text',[1,{a:1,b:2},[1,2]],{a:[1,2],b:{a:1,b:2}}];
 
-var stringifyJSON = function(obj, newStr) {
-	console.log("OBJ ",obj);
+var stringifyJSON = function(obj) {
 
-	newStr = newStr || "";
+	//console.log("stringify ",JSON.stringify(obj));
 
-	obj.forEach(function(ele,i,col){
+	if(obj === null){
+		return "null";
+	}else if (typeof obj === "number" || typeof obj === "boolean"){
+		return obj.toString();
+	}else if (typeof obj === "string"){
+		return '"'+ obj +'"';
 
-		//adds commas
-		if(i >= 1 && i <= col.length){
-			newStr += ",";
-		}
+	}else if (Array.isArray(obj)){
+		 var inside = "";
 
-		//if array
-		if(Array.isArray(ele)){
-			newStr += "[";
+		 	obj.forEach(function(ele,i,col){
+		 		if(i>=0 && i < col.length-1){
+		 			//console.log("middle ",inside);
+		 			inside += stringifyJSON(ele)+","
+		 		}else{
+		 			//console.log("ele ",ele);
+		 			inside += stringifyJSON(ele);
+		 			//console.log("inside ",inside);
+		 		}
 
-			stringifyJSON(ele,newStr);
+		 	});
+	//console.log("inside ",inside);
+		 return "[" + inside + "]";
 
-			newStr +=  "]";
-		}
+	}else if (typeof obj === "object"){
+		var inObj = "";
 
-		//if object
-		else if(typeof ele === "object"){
-			newStr += "{";
-
-			stringifyJSON(ele,newStr);
-
-			newStr += "}";
-		}
-
-		//regular expressions
-		else if(ele === undefined || ele === null){
-			newStr += "null";
-		}else if(ele === false){
-			newStr += "false";
-		}else if(ele === true){
-			newStr += "true";
-		}else if(typeof ele === "string"){
-			newStr += "\"" + ele + "\"";
-		}else if(typeof ele === "number"){
-			newStr += ele;
-		}
-	});
-	return newStr;
-
-
+			Object.keys(obj).forEach(function(key,i,col){
+				if (obj[key] !== undefined && obj[key] !== {} && !(obj[key] instanceof Function)){
+					inObj += stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+					if(i >= 0 && i < Object.keys(obj).length-1){
+						inObj += ",";
+					}
+				}
+			});
+		return "{" + inObj + "}";
+	}
 
 };
-	console.log("STRING TEST ",stringifyJSON(toStr));
-	console.log("STRINGIFY ",JSON.stringify(toStr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var toStr = [1,true,undefined,'text',[1,{a:1,b:2},[1,2]],{a:[1,2],b:{a:1,b:2}}];
+
+// var stringifyJSON = function(obj, newStr) {
+// 	console.log("OBJ ",obj);
+
+// 	newStr = newStr || "";
+
+//   if (Array.isArray(obj)){
+
+//     obj.forEach(function(ele,i,col){
+
+//       //adds commas
+//       if(i >= 1 && i <= col.length){
+//         newStr += ",";
+//       }
+
+//       //if array
+//       if(Array.isArray(ele)){
+//         newStr += "[";
+
+//         stringifyJSON(ele,newStr);
+
+//         newStr +=  "]";
+//       }
+
+//       //if object
+//       else if(typeof ele === "object"){
+//         newStr += "{";
+
+//         stringifyJSON(ele,newStr);
+
+//         newStr += "}";
+//       }
+
+//       //regular expressions
+//       else if(ele === undefined || ele === null){
+//         newStr += "null";
+//       }else if(ele === false){
+//         newStr += "false";
+//       }else if(ele === true){
+//         newStr += "true";
+//       }else if(typeof ele === "string"){
+//         newStr += "\"" + ele + "\"";
+//       }else if(typeof ele === "number"){
+//         newStr += ele;
+//       }
+//     });
+//   }
+
+//   if (obj instanceof Object){
+
+// 		var counter = 0;
+
+//     for(var key in obj){
+
+//       //adds commas
+//       if(counter >= 1 && counter <= obj.length){
+//         newStr += ",";
+//       }
+// 			counter++;
+//       //if array
+//       if(Array.isArray(obj[key])){
+//         newStr += "[";
+
+//         stringifyJSON(obj[key],newStr);
+
+//         newStr +=  "]";
+//       }
+
+//       //if object
+//       else if(typeof obj[key] === "object"){
+//         newStr += "{";
+
+//         stringifyJSON(obj[key],newStr);
+
+//         newStr += "}";
+//       }
+
+//       //regular expressions
+//       else if(obj[key] === undefined || obj[key] === null){
+//         newStr += "null";
+//       }else if(obj[key] === false){
+//         newStr += "false";
+//       }else if(obj[key] === true){
+//         newStr += "true";
+//       }else if(typeof obj[key] === "string"){
+//         newStr += "\"" + obj[key] + "\"";
+//       }else if(typeof obj[key] === "number"){
+//         newStr += obj[key];
+//       }
+//     }
+//   }
+//   return newStr;
+
+// };
+	// console.log("STRING TEST ",stringifyJSON(toStr));
+	// console.log("STRINGIFY ",JSON.stringify(toStr));
